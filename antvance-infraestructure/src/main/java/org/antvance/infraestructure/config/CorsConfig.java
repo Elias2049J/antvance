a9a6fbae757b8f9f435626749667394a3d1fb7c3
Henoc Elías Jorge Ugarte <henoceliasjorgeugarte@gmail.com>
2025-06-03 21:18:05 -0500

    Actualizar CorsConfig.java

--- antvance-infraestructure/src/main/java/org/antvance/infraestructure/config/CorsConfig.java
+++ antvance-infraestructure/src/main/java/org/antvance/infraestructure/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package org.antvance.infraestructure.config;// En algún paquete de configuración, por ejemplo org.antvance.infraestructure.config
+package org.antvance.infraestructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
